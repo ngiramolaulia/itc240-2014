3b30e2826f49d9fc1e2ae302b9fc686c20410aaf
Lia Ngiramolau <palauprincess.ln@gmail.com>
2014-04-14 17:38:42 -0700

    This is my Second Page

---
+++ assignment-01/second.php
@@ -0,0 +1,33 @@
+<?php $page_title = "Second Page";
+$page_head = "Volleyball";
+include ("header.php") ?>
+
+<h1>This is the Second Page</h1>
+<?php $middle = "Git Busy Assignment";
+echo $middle;?>
+
+<p>Etiam dictum varius orci nec dapibus. Nam tellus elit, tincidunt in 
+placerat at, dignissim nec eros. Phasellus tincidunt, elit sed 
+consectetur porttitor, enim tellus vulputate purus, elementum ullamcorper 
+nisi mauris eu felis. Suspendisse accumsan mauris eu lorem suscipit, in 
+mollis metus feugiat. Sed posuere non enim et dapibus. Proin sagittis 
+magna eget dui cursus tempus. Aliquam erat volutpat. Vestibulum justo 
+elit, dapibus sed lectus sed, mollis lobortis justo. Nam mattis, nisi ac 
+lacinia molestie, enim risus mattis ante, a cursus nulla tortor ac leo. 
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur 
+ridiculus mus.Duis luctus, tellus id imperdiet tempus, lorem velit 
+tempus neque, non mollis metus mi non nibh. Nam sagittis orci quis 
+suscipit faucibus. Proin posuere sem ligula, vitae congue massa 
+facilisis a. Vestibulum felis augue, dignissim in scelerisque eget, 
+commodo sit amet elit. Phasellus ultrices bibendum elit, at 
+condimentum leo. Ut leo sapien, faucibus a enim ut, hendrerit mattis 
+mauris. Praesent nec orci rutrum, imperdiet massa ultrices, sodales 
+metus. Fusce iaculis mi eros, vel scelerisque tellus fermentum eget. 
+Aliquam erat volutpat. Vestibulum iaculis nisi eget urna eleifend 
+tristique eu egestas elit. Morbi mi lacus, tincidunt dignissim elit 
+non, vehicula tempus magna. Quisque dignissim sapien orci, quis 
+rhoncus massa porta a. </p>
+
+<?php $page_footer = "Ngiramolau,Lia 2014";
+include ("footer.php");?>
+

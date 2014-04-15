60c1c99cf75e74f4ed33e65633ffdd7ae219d0e2
Lia Ngiramolau <palauprincess.ln@gmail.com>
2014-04-14 17:33:48 -0700

    First page

--- assignment-01/index.php
+++ assignment-01/index.php
@@ -1,3 +1,28 @@
-<!doctype>
-<html> 
-Hello World!
+<?php $page_title = "First Page";
+$page_head = "Basketbal";
+include ("header.php");?>
+
+<h1>This is the First page</h1>
+<?php $middle = "Git Busy Assignment";
+echo $middle;?>
+<p>Praesent consectetur velit sit amet facilisis facilisis. Cras a 
+hendrerit dui, a vulputate tortor. Quisque ullamcorper, tellus vitae 
+molestie consequat, lectus dui dignissim erat, ut volutpat nulla lectus 
+vel risus. Duis gravida justo at pulvinar ultricies. Maecenas tristique 
+pellentesque pharetra. Nullam vitae sapien id mi pretium condimentum. 
+Donec vitae neque nunc. Nunc tempus vitae purus et pretium. Duis eros 
+dui, venenatis vitae ante id, pulvinar adipiscing magna. Ut adipiscing 
+aliquet dolor, sed pretium diam bibendum id. Donec ut vulputate orci. 
+Duis sagittis, lectus ornare dictum luctus, neque nibh sodales metus, nec 
+euismod nisi tellus tempor est. Lorem ipsum dolor sit amet, consectetur 
+adipiscing elit. Cras tincidunt tincidunt dictum. Morbi sit amet velit 
+congue, aliquet eros sit amet, congue mi. Proin feugiat molestie mauris, 
+ac lacinia erat vehicula id. Duis purus leo, pharetra eu congue sit amet, 
+suscipit quis ante. Proin aliquam metus vel adipiscing volutpat. Morbi et 
+dui dapibus, fermentum metus quis, commodo lorem. Etiam in tortor massa. 
+Quisque convallis nisi arcu, nec laoreet sem egestas vel. Praesent ut 
+dignissim lectus. Fusce elementum condimentum odio, a pharetra est 
+adipiscing vitae. </p>
+
+<?php $page_footer = "Ngiramolau,Lia 2014";
+include ("footer.php");?>
